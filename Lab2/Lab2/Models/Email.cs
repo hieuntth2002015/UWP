c4ce73ed674aae590cdf92fb49f332300dba7411
Nguyen Trung HIeu <65000911+hieuntth2002015@users.noreply.github.com>
2021-05-30 23:04:15 +0700

    lab2

---
+++ Lab2/Lab2/Models/Email.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace Lab2.Models
+{
+    class Email
+    {
+        private string mailTo;
+        private string title;
+        private string content;
+        private TextBox emailAddress;
+        private TextBox title1;
+        private TextBox content1;
+
+        public Email(string mailTo, string title, string content)
+        {
+            MailTo = mailTo;
+            Title = title;
+            Content = content;
+
+        }
+
+        public Email(TextBox emailAddress, TextBox title1, TextBox content1)
+        {
+            this.emailAddress = emailAddress;
+            this.title1 = title1;
+            this.content1 = content1;
+        }
+
+        public string MailTo
+        {
+            get => mailTo;
+            set => mailTo = value;
+        }
+        public string Title
+        {
+            get => title;
+            set => title = value;
+        }
+        public string Content
+        {
+            get => content;
+            set => content = value;
+        }
+    }
+}
